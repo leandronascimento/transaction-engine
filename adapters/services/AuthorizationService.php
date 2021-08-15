30aa1efde189bf12adf09eb1628090892e3b8315
Leandro Nascimento <leandro.nascimento@petco.com>
2021-08-15 03:53:03 -0300

    authorization service

--- adapters/services/AuthorizationService.php
+++ adapters/services/AuthorizationService.php
@@ -5,13 +5,28 @@ declare(strict_types=1);
 namespace Adapters\Services;
 
 use Domain\Contracts\AuthorizationTransactionService;
+use Exception;
+use GuzzleHttp\Client;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthorizationService implements AuthorizationTransactionService
 {
+    const URL = 'https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6';
 
     public function isAuthorized(): bool
     {
-        
-        return true;
+        try {
+            $client = new Client();
+            $response = $client->get(self::URL);
+            $body = json_decode($response->getBody()->getContents());
+
+            if ($response->getStatusCode() === Response::HTTP_OK && $body->message === 'Autorizado') {
+                return true;
+            };
+
+            return false;
+        } catch (Exception) {
+            throw new Exception('Unavailable service!');
+        }
     }
 }
